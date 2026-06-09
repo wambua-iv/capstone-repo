9171cdb7d1910d8ed65c43d2e171646f641ac684
Wambua Musalu <musaluwambua@gmail.com>
2026-06-09 13:15:01 +0300

    re-platform

--- main.tf
+++ main.tf
@@ -281,8 +281,8 @@ resource "azurerm_linux_virtual_machine" "web_server" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "24_04-lts-gen2"
+    offer     = "ubuntu-24_04-lts" 
+    sku       = "server"           
     version   = "latest"
   }
   os_disk {
