62cf6589dd6fbf55c55cb272a1ddbf42ff7b503d
Wambua Musalu <musaluwambua@gmail.com>
2026-06-12 01:31:06 +0300

    run presentation

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_iam_role" "ec2_execute_role" {
-  name = "capstone-ec2-execute-role"
+  name = "capstone-ec2-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -37,7 +37,7 @@ resource "aws_iam_role" "ec2_execute_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "capstone-ec2-instance-profile"
+  name = "capstone-ec2-profile"
   role = aws_iam_role.ec2_execute_role.name
 }
 
